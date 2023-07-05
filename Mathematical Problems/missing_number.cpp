db1750169f3d80f6f52f7142db206289c446e05e
ShivamGupta <114949334+Shivam86041@users.noreply.github.com>
2023-07-05 23:26:38 +0530

    Initial Commit

---
+++ Mathematical Problems/missing_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0;i<=nums.size();i++){
+            ans = ans ^ i;
+        }
+        for(int i = 0;i<nums.size();i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
