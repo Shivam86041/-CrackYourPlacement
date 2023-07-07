4038049598b7a1f183dcfc472659ff6f711f4790
ShivamGupta <114949334+Shivam86041@users.noreply.github.com>
2023-07-07 23:09:03 +0530

    Initial Commit

---
+++ Mathematical Problems/maximum_product_of_three_numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return max(nums[0]*nums[1]*nums[n-1], nums[n-1]*nums[n-2]*nums[n-3]);
+    }
+};
